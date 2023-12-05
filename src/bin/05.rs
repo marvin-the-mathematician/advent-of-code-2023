400efc26825aa91834a3a9460247bc4a1b7dcc48
Marvin Jones <marvin.jones@five.ai>
2023-12-05 22:35:43 +0000

    Parse Almanac for Day 5, Part 1.

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -1,10 +1,165 @@
 advent_of_code::solution!(5);
 
-pub fn part_one(_input: &str) -> Option<u32> {
-    None
+use nom::{
+    bytes::complete::tag,
+    character::complete::{alpha1, char, digit1, space1},
+    combinator::map_res,
+    error::Error,
+    multi::separated_list1,
+    sequence::{delimited, separated_pair},
+    Finish, IResult,
+};
+use std::str::FromStr;
+
+// almanac
+// map
+// source category
+// destination category
+// source number
+// desination number
+// ranges
+// range_start
+// range_length
+// A span (range_start, range_length)...
+// source range start
+// destination range start
+// Any source numbers that aren't mapped correspond to the same destination number.
+// the lowest "location" number that corresponds to any of the initial "seeds".
+
+type Distance = u64;
+type Descriptor = u64;
+type Descriptors = Vec<Descriptor>;
+
+fn parse_distance(input: &str) -> IResult<&str, Descriptor> {
+    let (i, distance) = map_res(digit1, str::parse)(input)?;
+    Ok((i, distance))
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
+fn parse_descriptor(input: &str) -> IResult<&str, Descriptor> {
+    let (i, descriptor) = map_res(digit1, str::parse)(input)?;
+    Ok((i, descriptor))
+}
+
+#[derive(Debug, PartialEq)]
+struct Range {
+    source_start: Descriptor,
+    destination_start: Descriptor,
+    length: Distance,
+}
+
+fn parse_range(input: &str) -> IResult<&str, Range> {
+    // 50 98 2
+    let (i, (destination_start, (source_start, length))) = separated_pair(
+        parse_descriptor,
+        char(' '),
+        separated_pair(parse_descriptor, char(' '), parse_distance),
+    )(input)?;
+    Ok((
+        i,
+        Range {
+            source_start,
+            destination_start,
+            length,
+        },
+    ))
+}
+
+impl Range {
+    fn _maybe_destination(&self, source: Descriptor) -> Option<Descriptor> {
+        if source >= self.source_start && source < self.source_start + self.length {
+            Some(self.destination_start + source - self.source_start)
+        } else {
+            None
+        }
+    }
+}
+
+type Ranges = Vec<Range>;
+
+fn parse_ranges(input: &str) -> IResult<&str, Ranges> {
+    // 50 98 2\n52 50 48
+    let (i, ranges) = separated_list1(char('\n'), parse_range)(input)?;
+    Ok((i, ranges))
+}
+
+#[derive(Debug, PartialEq)]
+struct Map {
+    source_category: String,
+    destination_category: String,
+    ranges: Ranges,
+}
+
+fn parse_map(input: &str) -> IResult<&str, Map> {
+    // seed-to-soil map:\n50 98 2\n52 50 48
+    let (i, (source_category, destination_category)) =
+        separated_pair(alpha1, tag("-to-"), alpha1)(input)?;
+    let (i, _) = tag(" map:\n")(i)?;
+    let (i, ranges) = parse_ranges(i)?;
+    Ok((
+        i,
+        Map {
+            source_category: source_category.to_string(),
+            destination_category: destination_category.to_string(),
+            ranges,
+        },
+    ))
+}
+
+type Maps = Vec<Map>;
+
+fn parse_maps(input: &str) -> IResult<&str, Maps> {
+    // seed-to-soil map:\n50 98 2\n52 50 48\n\nsoil-to-fertilizer map:\n0 15 37\n37 52 2
+    let (i, maps) = separated_list1(tag("\n\n"), parse_map)(input)?;
+    Ok((i, maps))
+}
+
+#[derive(Debug, PartialEq)]
+struct Almanac {
+    seeds: Descriptors,
+    maps: Maps,
+}
+
+fn parse_seeds(input: &str) -> IResult<&str, Descriptors> {
+    // seeds: 3139431799 50198205 3647185634\n\n
+    let (i, descriptors) = delimited(
+        tag("seeds: "),
+        separated_list1(space1, parse_descriptor),
+        tag("\n\n"),
+    )(input)?;
+    Ok((i, descriptors))
+}
+
+fn parse_almanac(input: &str) -> IResult<&str, Almanac> {
+    // seeds: 3139431799 50198205 3647185634\n\nseed-to-soil map:\n50 98 2\n52 50 48\n\nsoil-to-fertilizer map:\n0 15 37\n37 52 2
+    let (i, seeds) = parse_seeds(input)?;
+    let (i, maps) = parse_maps(i)?;
+    Ok((i, Almanac { seeds, maps }))
+}
+
+impl FromStr for Almanac {
+    type Err = Error<String>;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match parse_almanac(s).finish() {
+            Ok((_, almanac)) => Ok(almanac),
+            Err(Error { input, code }) => Err(Error {
+                input: input.to_string(),
+                code,
+            }),
+        }
+    }
+}
+
+pub fn part_one(input: &str) -> Option<Descriptor> {
+    let almanac = Almanac::from_str(input).ok()?;
+    println!("{:?}", almanac);
+
+    let nearest_location = almanac.seeds.into_iter().map(|_seed| 0u64).min()?;
+
+    Some(nearest_location)
+}
+
+pub fn part_two(_input: &str) -> Option<Descriptor> {
     None
 }
 
@@ -15,7 +170,7 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(35));
     }
 
     #[test]

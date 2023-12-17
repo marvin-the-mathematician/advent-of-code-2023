dd1edf7514cb07d446df90e3c3ab16f6b9b5abed
Marvin Jones <marvin.jones@five.ai>
2023-12-17 18:41:38 +0000

    Solve Day 17, Part 1.

--- src/bin/17.rs
+++ src/bin/17.rs
@@ -10,13 +10,15 @@ use nom::{
     multi::{many1, separated_list1},
     Finish, IResult,
 };
+use pathfinding::directed::dijkstra::dijkstra;
+use std::iter::once;
 use std::str::FromStr;
 
-type Loss = u32;
+type Cost = u32;
 
 #[derive(Copy, Clone, Debug, Hash, Ord, PartialOrd, Eq, PartialEq)]
 struct Block {
-    loss: Loss,
+    cost: Cost,
 }
 
 fn parse_block(input: &str) -> IResult<&str, Block> {
@@ -34,7 +36,7 @@ fn parse_block(input: &str) -> IResult<&str, Block> {
     Ok((
         i,
         Block {
-            loss: c.to_digit(10).unwrap(),
+            cost: c.to_digit(10).unwrap(),
         },
     ))
 }
@@ -56,11 +58,34 @@ fn parse_streets(input: &str) -> IResult<&str, Streets> {
 type Blocks = Array<Block, Ix2>;
 type Index = [usize; 2];
 
+#[derive(Copy, Clone, Debug, Hash, Ord, PartialOrd, Eq, PartialEq)]
+enum Heading {
+    North,
+    South,
+    East,
+    West,
+}
+
+type RunLength = u32;
+
+#[derive(Copy, Clone, Debug, Hash, Eq, PartialEq)]
+struct State {
+    index: Index,
+    heading: Heading,
+    run_length: RunLength,
+}
+
+type MaybeState = Option<State>;
+type CostedState = (State, Cost);
+type CostedStates = Vec<CostedState>;
+
 #[derive(Debug)]
 struct City {
     street_count: usize,
     avenue_count: usize,
     blocks: Blocks,
+    start_index: Index,
+    goal_index: Index,
 }
 
 fn parse_city(input: &str) -> IResult<&str, City> {
@@ -85,6 +110,8 @@ fn parse_city(input: &str) -> IResult<&str, City> {
             street_count,
             avenue_count,
             blocks,
+            start_index: [0, 0],
+            goal_index: [street_count - 1, avenue_count - 1],
         },
     ))
 }
@@ -103,11 +130,143 @@ impl FromStr for City {
     }
 }
 
-pub fn part_one(input: &str) -> Option<Loss> {
-    let city = City::from_str(input).ok()?;
-    println!("{:?}", city);
+impl City {
+    fn block_at(&self, index: Index) -> &Block {
+        return &self.blocks[index];
+    }
 
-    Some(0)
+    fn maybe_state_ahead(&self, state: &State) -> MaybeState {
+        let [i, j] = state.index;
+        // Can continue ahead at run-lengths 1 and 2.
+        // Must turn at run-length 3.
+        // Otherwise, panic!
+        match state.run_length {
+            run_length @ 1..=2 => match state.heading {
+                Heading::North if i > 0 => Some(State {
+                    index: [i - 1, j],
+                    heading: Heading::North,
+                    run_length: run_length + 1,
+                }),
+                Heading::South if i < self.street_count - 1 => Some(State {
+                    index: [i + 1, j],
+                    heading: Heading::South,
+                    run_length: run_length + 1,
+                }),
+                Heading::East if j < self.avenue_count - 1 => Some(State {
+                    index: [i, j + 1],
+                    heading: Heading::East,
+                    run_length: run_length + 1,
+                }),
+                Heading::West if j > 0 => Some(State {
+                    index: [i, j - 1],
+                    heading: Heading::West,
+                    run_length: run_length + 1,
+                }),
+                _ => None,
+            },
+            3 => None,
+            _ => panic!(),
+        }
+    }
+
+    fn maybe_state_on_left(&self, state: &State) -> MaybeState {
+        let [i, j] = state.index;
+        match state.heading {
+            Heading::North if j > 0 => Some(State {
+                index: [i, j - 1],
+                heading: Heading::West,
+                run_length: 1,
+            }),
+            Heading::South if j < self.avenue_count - 1 => Some(State {
+                index: [i, j + 1],
+                heading: Heading::East,
+                run_length: 1,
+            }),
+            Heading::East if i > 0 => Some(State {
+                index: [i - 1, j],
+                heading: Heading::North,
+                run_length: 1,
+            }),
+            Heading::West if i < self.street_count - 1 => Some(State {
+                index: [i + 1, j],
+                heading: Heading::South,
+                run_length: 1,
+            }),
+            _ => None,
+        }
+    }
+
+    fn maybe_state_on_right(&self, state: &State) -> MaybeState {
+        let [i, j] = state.index;
+        match state.heading {
+            Heading::North if j < self.avenue_count - 1 => Some(State {
+                index: [i, j + 1],
+                heading: Heading::East,
+                run_length: 1,
+            }),
+            Heading::South if j > 0 => Some(State {
+                index: [i, j - 1],
+                heading: Heading::West,
+                run_length: 1,
+            }),
+            Heading::East if i < self.street_count - 1 => Some(State {
+                index: [i + 1, j],
+                heading: Heading::South,
+                run_length: 1,
+            }),
+            Heading::West if i > 0 => Some(State {
+                index: [i - 1, j],
+                heading: Heading::North,
+                run_length: 1,
+            }),
+            _ => None,
+        }
+    }
+
+    fn next_states_with_losses(&self, state: &State) -> CostedStates {
+        once(self.maybe_state_ahead(state))
+            .chain(once(self.maybe_state_on_left(state)))
+            .chain(once(self.maybe_state_on_right(state)))
+            .flatten()
+            .map(|state| (state, self.block_at(state.index).cost))
+            .collect()
+    }
+
+    fn did_reach_goal(&self, state: &State) -> bool {
+        state.index == self.goal_index
+    }
+}
+
+pub fn part_one(input: &str) -> Option<Cost> {
+    let city = City::from_str(input).ok()?;
+    // println!("{:?}", city);
+
+    let start_state_heading_east = State {
+        index: city.start_index,
+        heading: Heading::East,
+        run_length: 1,
+    };
+    let (_, minimal_loss_heading_east) = dijkstra(
+        &start_state_heading_east,
+        |state| city.next_states_with_losses(state),
+        |state| city.did_reach_goal(state),
+    )?;
+
+    let start_state_heading_south = State {
+        index: city.start_index,
+        heading: Heading::South,
+        run_length: 1,
+    };
+    let (_, minimal_loss_heading_south) = dijkstra(
+        &start_state_heading_south,
+        |state| city.next_states_with_losses(state),
+        |state| city.did_reach_goal(state),
+    )?;
+
+    Some(std::cmp::min(
+        minimal_loss_heading_east,
+        minimal_loss_heading_south,
+    ))
 }
 
 pub fn part_two(_input: &str) -> Option<u32> {
